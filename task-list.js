94fccc3dafab410f063eea43fadae122d43d0233
ISDI <isdi@ISDIs-MacBook-Air.local>
2018-02-10 14:33:59 -0600

    Avances de Feb 10

--- task-list.js
+++ task-list.js
@@ -1,19 +1,30 @@
+var categorias = [
+{"value":"success","lable":"verduras"},
+{"value":"warning","label":"comida chatarra"},
+{"value":"info","lablr":"Frutas"},
+{"value":"secondart","lable":"Alcohol"},
+]
+
 $(document).ready(function(){
   var $formulario_de_lista = $('#formulario-de-lista');
   var $lista_principal = $('#lista-principal');
-  var $tarea = $('input#tarea');
+  var $producto = $('input#producto');
+  var $categoria = $('select#categoria');
 
   var template = '<li class="list-group-item list-group-item-action"></li>';
 
   $formulario_de_lista.on('submit', function( event ){
     event.preventDefault();
-    var texto_de_tarea = $tarea.val().trim();
+    var texto_de_producto = $producto.val().trim();
+    var categoria = $categoria.find('option:selected').index();
 
-    if( texto_de_tarea ){
+
+    if( texto_de_producto ){
+      var clase = categorias[ categoria];
 
       var $new_row = $(
-        '<li class="list-group-item list-group-item-action">' +
-      texto_de_tarea +
+        '<li class="list-group-item text-'+clase.value+' list-group-item-action">' +
+      texto_de_producto +
       '</li>'
     );
       $lista_principal.append( $new_row );
@@ -21,7 +32,7 @@ $(document).ready(function(){
       $new_row.hide().fadeIn();
     }
 
-    $tarea.val('');
+    $producto.val('');
 
     // alert(texto_de_tarea )
   })
